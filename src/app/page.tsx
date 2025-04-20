647e50e496c404c64d7e6a7748bac021360e829b
Aubrey Barnes <aubrey.hbarnes@gmail.com>
2025-04-20 09:05:23 -0500

    adding all images/icons, styling as close to design as possible

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,69 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid items-center justify-items-center p-4 md:p-8 pb-20 sm:p-20 font-[family-name:var(--font-noto-sans)]">
+      <section className="intro-copy-container flex flex-col items-center gap-4">
+        <h1 className="intro-subhead text-indigo-700 text-center font-semibold">High Quality Images</h1>
+        <h2 className="intro-heading text-center text-3xl md:text-5xl font-semibold">For Designers, By Designers</h2>
+        <p className="intro-copy text-center max-w-[52ch] text-neutral-600">Unleash boundless creativity with a large repository of images optimized for designers</p>
+      </section>
+      <section className="2-col-feature-section flex flex-col lg:flex-row ">
+        <div className="feature-list pt-16 md:px-4 lg:w-1/2">
+          <div className="icon-row pb-16 flex flex-row gap-2">
+            <div className="icon-wrapper min-w-[48px]">
+              <Image
+                src={'/HD-Icon.svg'}
+                alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <div className="icon-copy-wrapper">
+              <h3 className="font-semibold">5K resolution support</h3>
+              <p>All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality.</p>
+            </div>
+          </div>
+          <div className="icon-row pb-16 flex flex-row gap-2">
+            <div className="icon-wrapper min-w-[48px]">
+              <Image
+                src={'/water-icon.svg'}
+                alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <div className="icon-copy-wrapper">
+            <h3 className="font-semibold">From water to glass</h3>
+              <p>We offer a wide array of abstractions, ranging from water to glass, 
+                 and encompassing various styles including 3D and vector.</p>
+            </div>
+          </div>
+          <div className="icon-row pb-16 flex flex-row gap-2">
+            <div className="icon-wrapper min-w-[48px]">
+              <Image
+                src={'/orientation-icon.svg'}
+                alt=""
+                width={50}
+                height={50}
+                />
+            </div>
+            <div className="icon-copy-wrapper">
+            <h3 className="font-semibold">Portrait or landscape</h3>
+              <p>Effortlessly adapt your images for any platform - 
+                 whether it's a stunning wallpaper or captivating Instagram reels and stories.</p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="feature-img lg:w-1/2 relative min-h-[180px] flex items-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={'/unsplash_rAtzDB6hWrU.jpg'}
+            width={480}
+            height={180}
+            alt=""
+            className="cover rounded-md w-full min-h-[180px]"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </section>
     </div>
   );
 }
